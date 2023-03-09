93a3a0f63ab2cc4c981ec18ef57d91d1f2565867
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2023-03-09 19:45:29 +0100

    Update testMessage.js

--- src/events/testMessage.js
+++ src/events/testMessage.js
@@ -1,6 +1,6 @@
 const EventListener = require('../../../../src/modules/events/manager.js');
 
-module.exports = class DebugEventListener extends EventListener {
+module.exports = class exampleEvent extends EventListener {
 	constructor(client) {
 		super(client, { event: 'messageCreate', once: false, name: null, default: false });
 	}

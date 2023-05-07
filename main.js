12d582d2af0d6e4a982d452585e53d22a1ae2b6c
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2023-05-07 13:12:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const pluginManager = require('../../src/modules/plugins/manager.js');
 const devConfig = require('./dev.json');
+// const manager = require('./manager.js')
 
 module.exports = class examplePlugin extends pluginManager {
 	constructor(client) {
-		super(client, devConfig);
+		super(client, devConfig, manager);
 	}
 };

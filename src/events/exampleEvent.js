0be60c24fe86058be97cdf2278026bc620634e8b
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2023-05-07 13:16:09 +0200

    Update exampleEvent.js

--- src/events/exampleEvent.js
+++ src/events/exampleEvent.js
@@ -1,8 +1,9 @@
 const EventListener = require('../../../../src/modules/events/manager.js');
+const { Events } = require('discord.js');
 
 module.exports = class exampleEvent extends EventListener {
 	constructor(client) {
-		super(client, { event: 'messageCreate', once: false, name: 'Example', default: false, plugin: 'general' }); // default: used for interaction events, keep it as "false", use "true" to bypass custom interaction handler
+		super(client, { event: Events.MessageCreate, once: false, name: 'Example', default: false, plugin: 'general' }); // default: used for interaction events, keep it as "false", use "true" to bypass custom interaction handler
 	}
 
 	async execute(message) {

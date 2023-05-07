04b0c0ddcefb65fd64cb51b27aa72023d72d8dd1
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2023-05-07 13:15:40 +0200

    Update exampleEvent.js

--- src/events/exampleEvent.js
+++ src/events/exampleEvent.js
@@ -2,7 +2,7 @@ const EventListener = require('../../../../src/modules/events/manager.js');
 
 module.exports = class exampleEvent extends EventListener {
 	constructor(client) {
-		super(client, { event: 'messageCreate', once: false, name: null, default: false });
+		super(client, { event: 'messageCreate', once: false, name: 'Example', default: false, plugin: 'general' }); // default: used for interaction events, keep it as "false", use "true" to bypass custom interaction handler
 	}
 
 	async execute(message) {
